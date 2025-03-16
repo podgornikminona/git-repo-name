f0dcfa8a0e5bc260230a4c3c4c2d63eb2487050e
podgornikminona <podgornikminona@gmail.com>
2025-03-16 08:00:49 +0000

    Add 11.js

---
+++ src/11.js
@@ -0,0 +1,7 @@
+// Get a random number between 1 and 10
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 10) + 1;
+};
+
+// Use the function to get a random number
+console.log(getRandomNumber());
