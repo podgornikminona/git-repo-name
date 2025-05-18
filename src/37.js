b21db0ac6892eedd55b8be1859d160ee16faf4d0
podgornikminona <podgornikminona@gmail.com>
2025-05-18 22:46:27 +0000

    Add 37.js

---
+++ src/37.js
@@ -0,0 +1,7 @@
+// This is a random JavaScript code to demonstrate a simple function and variable usage.
+function add(a, b) {
+  return a + b;
+}
+
+const result = add(2, 3);
+console.log(result); // Output: 5
